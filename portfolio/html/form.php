6f91eee312ceebff132b938be3942a886e70c36c
Soumaia Mamo <75613677+Soumaia-Mamo@users.noreply.github.com>
2021-12-30 20:41:48 +0100

    Add files via upload

---
+++ portfolio/html/form.php
@@ -0,0 +1,21 @@
+<!-- 
+<?php echo $_GET["voornaam"]; ?><br>
+<?php echo $_GET["email"]; ?>
+ 
+<!DOCTYPE html>
+<html>
+  <body>
+
+Ingediende formuliergegevens
+Uw input is ontvangen als:
+
+<?php
+echo $_SERVER['voornaam'];
+?>
+
+</body>
+</html> -->
+
+<?php
+echo "Hello World!";
+?>
